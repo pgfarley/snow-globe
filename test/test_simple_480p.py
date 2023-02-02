f72a7a0866deafe4df39ebc5ae9542c7ebd1b1b7
pgfarley <9804+pgfarley@users.noreply.github.com>
2023-02-02 12:37:07 +0000

    test clean up

--- test/test_simple_480p.py
+++ test/test_simple_480p.py
@@ -3,15 +3,31 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles, with_timeout
 
 
+async def reset(dut):
+    dut.rst_pix.value =  1
+    await RisingEdge(dut.clk_pix)
+
+    dut.rst_pix.value = 0
+    await RisingEdge(dut.clk_pix)
+
+    await RisingEdge(dut.clk_pix)
+
+def is_vsync(dut) -> bool:
+    return  dut.vsync.value.integer == 0 # active low: vsync
+
+def is_visible(dut) -> bool:
+    return dut.de.value.integer == 1
+
+
 @cocotb.test()
 async def test_max_active_positions(dut):
 
-    H_LAST_POSITION = dut.H_ACTIVE.value \
+    expected_max_horizontal_position = dut.H_ACTIVE.value \
     + dut.H_FRONT_PORCH.value \
     + dut.H_SYNC_PULSE.value \
     + dut.H_BACK_PORCH.value - 1
 
-    V_LAST_POSITION = dut.V_ACTIVE.value \
+    expected_max_vertical_position = dut.V_ACTIVE.value \
     + dut.V_FRONT_PORCH.value \
     + dut.V_SYNC_PULSE.value \
     + dut.V_BACK_PORCH.value - 1
@@ -19,35 +35,21 @@ async def test_max_active_positions(dut):
 
     clock = Clock(dut.clk_pix, 1, units="us")
     cocotb.start_soon(clock.start())
-    
-    dut.rst_pix.value = 1
-    await RisingEdge(dut.clk_pix)
 
-    dut.rst_pix.value = 0
-    await RisingEdge(dut.clk_pix)
-    await RisingEdge(dut.clk_pix)
+    await reset(dut)
 
-    highest_horizontal_position = -1
-    highest_vertical_position = -1
+    actual_max_horizontal_position = -1
+    actual_max_vertical_position = -1
+    one_vsync_seen = False
 
-    while dut.vsync.value.integer == 1: # active low: Not yet vsync
-        highest_horizontal_position = max(dut.sx.value.integer, highest_horizontal_position)
-        highest_vertical_position = max(dut.sy.value.integer, highest_vertical_position)
+    while not (one_vsync_seen and is_visible(dut)):
+        one_vsync_seen = one_vsync_seen or is_vsync(dut)
+        actual_max_horizontal_position = max(dut.sx.value.integer, actual_max_horizontal_position)
+        actual_max_vertical_position = max(dut.sy.value.integer, actual_max_vertical_position)
         await RisingEdge(dut.clk_pix)
 
-    
-    while dut.vsync.value.integer == 0: # active low: vsync
-        highest_horizontal_position = max(dut.sx.value.integer, highest_horizontal_position)
-        highest_vertical_position = max(dut.sy.value.integer, highest_vertical_position)
-        await RisingEdge(dut.clk_pix)
-
-    while dut.de.value.integer == 0: # vertical back porch
-        highest_horizontal_position = max(dut.sx.value.integer, highest_horizontal_position)
-        highest_vertical_position = max(dut.sy.value.integer, highest_vertical_position)
-        await RisingEdge(dut.clk_pix)
-
-    assert highest_horizontal_position == H_LAST_POSITION
-    assert highest_vertical_position == V_LAST_POSITION
+    assert expected_max_horizontal_position == actual_max_horizontal_position
+    assert expected_max_vertical_position == actual_max_vertical_position
 
 
 
